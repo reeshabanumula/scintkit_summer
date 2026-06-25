bd16e8bfbe8e6f1ee13e4841f1848403ef2dd0fd
Reeshab Anumula <reeshab.anumula@gmail.com>
2026-06-25 14:01:58 -0500

    organize functions and create v2 of compute cross cor

--- src/scintkit/space_receiver_processing/functions.py
+++ src/scintkit/space_receiver_processing/functions.py
@@ -19,3 +19,16 @@ def cross_correlation(sig1, sig2):
     best_lag = lag_norm[np.argmax(cor_norm)]
     
     return max_corr, best_lag, cor_norm, lag_norm
+
+
+# helps create s4
+def db2lin(sig_db):
+    return 10 ** (sig_db / 10)
+
+
+def datetime_to_seconds(df, time_column="datetime"):
+    df = df.copy()
+
+    # Seconds since first measurement
+    df["time_sec"] = (df[time_column] - df[time_column].iloc[0]).dt.total_seconds()
+    return df
